90e71b6f7f53f76107f896d3498acb626ca77eb0
huiger <zhihuiemail@163.com>
2017-11-26 13:37:52 +0800

    测试一下分支

--- app/src/main/java/top/huiger/testproject/MainActivity.java
+++ app/src/main/java/top/huiger/testproject/MainActivity.java
@@ -4,8 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * MainActivity
+ *
+ * Created by <lzh> on 2017/11/26.
+ */
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
